7f289b26d13e023905ac2a9eff43c741882e4553
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-10-17 14:06:54 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-console.log("Menyalakan mesin kopi");
-console.log("Menggiling biji kopi");
-console.log("Memanaskan air");
-console.log("Mencampurkan air dan kopi");
-console.log("Menuangkan kopi ke dalam gelas");
-console.log("Menuangkan susu ke dalam gelas");
-console.log("Kopi Anda sudah siap!");
+// console.log("Menyalakan mesin kopi");
+// console.log("Menggiling biji kopi");
+// console.log("Memanaskan air");
+// console.log("Mencampurkan air dan kopi");
+// console.log("Menuangkan kopi ke dalam gelas");
+// console.log("Menuangkan susu ke dalam gelas");
+// console.log("Kopi Anda sudah siap!");
+
+const coffeStock = require('./state');
+
+const makeCoffe = (type, miligrams) => {
+    if(coffeStock[type] >= miligrams) {
+        console.log("kopi berhasil dibuat!");
+    } else {
+        console.log("Biji kopi habis");
+    }
+}
+
+makeCoffe('liberica', 100);
