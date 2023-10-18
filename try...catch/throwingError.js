46afd4655208bd9a63e36803ba39d333b4aef7d0
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-10-18 13:45:05 +0700

    handle SyntaxError

--- try...catch/throwingError.js
+++ try...catch/throwingError.js
@@ -1,8 +1,13 @@
 // const json = '{ "name": "Yoda", "age": 20 }';
-const json = '{ bad json }';
+// const json = '{ bad json }';
+const json = '{ "age": 20 }';
 
 try {
     const user = JSON.parse(json);
+
+    if(!user.name) {
+        throw new SyntaxError("'name' is required");
+    }
 
     console.log(user.name);
     console.log(user.age);
