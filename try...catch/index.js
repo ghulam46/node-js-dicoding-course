eca01e38a726b98db4af7690b1dbd677dc6b39e8
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-10-18 11:32:00 +0700

    try...catch

---
+++ try...catch/index.js
@@ -0,0 +1,9 @@
+try {
+    console.log("Awal blok try");
+    errorCode;
+    console.log("Akhir blok try");
+  } catch (error) {
+    console.log('error name: '+ error.name);
+    console.log('error message: '+ error.message);
+    console.log('error stack: '+ error.stack);
+  }
