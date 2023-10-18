fac302ed3517b745b67fd20de182692ecb138690
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-10-18 15:18:19 +0700

    update

--- try...catch/throwingError.js
+++ try...catch/throwingError.js
@@ -14,6 +14,11 @@ try {
     console.log(user.name);
     console.log(user.age);
 } catch(error) {
-    // console.log(error.name);
-    console.log(`JSON Error: ${error.message}`);
+    if(error instanceof SyntaxError) {
+        console.log(`JSON Error: ${error.message}`);
+    } else if(error instanceof ReferenceError) {
+        console.log(error.message);
+    } else {
+        console.log(error.stack);
+    }
 }
