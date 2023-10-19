ac1295b379cf16b69989dff6f1cb39202cde92dc
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-10-19 15:01:29 +0700

    handle error

--- handleError/error2.js
+++ handleError/error2.js
@@ -1,4 +1,8 @@
 const getEvenNumber = (arr) => {
+    if(!(typeof arr === 'object')) {
+        throw new Error('Parameter harus sebuah object!');
+    }
+
     const filteredArr = arr.filter((item) => {
         return item % 2 === 0;
     });
@@ -7,4 +11,5 @@ const getEvenNumber = (arr) => {
 
 const myArr = [1,2,3,4,5,6,7,8,9,10];
 
-console.log(getEvenNumber(myArr));
+// console.log(getEvenNumber(myArr)); // object
+console.log(getEvenNumber("myArr")); // bukan object
