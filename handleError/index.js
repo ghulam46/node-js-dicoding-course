90a41aabf8bf8f1b8443d51e4cc94bdcb70c4a9e
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-10-19 11:46:00 +0700

    handle error

---
+++ handleError/index.js
@@ -0,0 +1,40 @@
+class ValidationError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'ValidationError';
+    }
+}
+
+function validateNumberInput(a, b, c) {
+    if(typeof a !== 'number') {
+        throw new ValidationError('Argumen pertama harus number');
+    }
+    if(typeof b !== 'number') {
+        throw new ValidationError('Argumen kedua harus number');
+    }
+    if(typeof c !== 'number') {
+        throw new ValidationError('Argumen ketiga harus number');
+    }
+}
+
+const detectTriangle = (a, b, c) => {
+    try {
+        validateNumberInput(a, b, c);
+    } catch(error) {
+        return error.message;
+    }
+
+    if(a === b && b === c){
+        return 'segitiga sama sisi';
+    }
+
+    if (a === b || a === c || b === c) {
+        return 'Segitiga sama kaki';
+    }
+        
+    return 'Segitiga sembarang';
+}
+
+console.log(detectTriangle(1,1,1));
+console.log(detectTriangle(5,3,5));
+console.log(detectTriangle(12,2,145));
