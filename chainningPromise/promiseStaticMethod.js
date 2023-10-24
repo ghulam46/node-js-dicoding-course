b2d86d86eb6286e141273608cf1e7bc1a7941bdd
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-10-24 15:29:32 +0700

    Promise.all with reject

---
+++ chainningPromise/promiseStaticMethod.js
@@ -0,0 +1,7 @@
+const promise1 = new Promise((resolve) => setTimeout(() => resolve(1), 1000));
+const promise2 = new Promise((resolve, reject) => setTimeout(() => reject(new Error('ups')), 2000));
+const promise3 = new Promise((resolve) => setTimeout(() => resolve(3), 3000));
+
+Promise.all([promise1, promise2, promise3])
+    .then((values) => console.log(values))
+    .catch((error) => console.log(error.message));
