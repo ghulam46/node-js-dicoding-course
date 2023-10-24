ae30b67974f2528f4d15637c9624886bdcb12b6f
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-10-24 13:49:53 +0700

    chainning promise

---
+++ chainningPromise/chainningPromise.js
@@ -0,0 +1,53 @@
+function withDrawMoney(amount) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(amount > 100) {
+        reject(new Error('Not enough money to withdraw'));
+      }
+
+      resolve(amount);
+    }, 1000);
+  });
+}
+
+function buyCinemaTicket(money) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(money < 10) {
+        reject(new Error('Not enough money to buy ticket'));
+      }
+
+      resolve('ticket-1');
+    }, 1000);
+  });
+}
+
+function goInsideCinema(ticket) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(!ticket) {
+        reject(new Error('Not enough money to buy ticket'));
+      }
+
+      resolve('Enjoy the movie');
+    }, 1000);
+  });
+}
+
+function watchMovie() {
+  buyCinemaTicket(10)
+    .then((money) => {
+      return buyCinemaTicket(money);
+    })
+    .then((ticket) => {
+      return goInsideCinema(ticket);
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
+
+watchMovie();
