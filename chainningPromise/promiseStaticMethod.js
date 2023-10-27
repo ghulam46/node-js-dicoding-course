773b2bf63ff183441cb51844f900af04a8864c84
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-10-27 11:26:27 +0700

    promise race if reject

--- chainningPromise/promiseStaticMethod.js
+++ chainningPromise/promiseStaticMethod.js
@@ -6,9 +6,17 @@
 //     .then((values) => console.log(values))
 //     .catch((error) => console.log(error.message));
 
-const promise1 = new Promise((resolve) => setTimeout(() => resolve(1), 1000));
+// const promise1 = new Promise((resolve) => setTimeout(() => resolve(1), 1000));
+// const promise2 = new Promise((resolve) => setTimeout(() => resolve(2), 2000));
+// const promise3 = new Promise((resolve) => setTimeout(() => resolve(3), 3000));
+
+// Promise.race([promise1, promise2, promise3])
+//   .then((value) => console.log(value)); // 1 setelah 1 detik
+
+const promise1 = new Promise((resolve, reject) => setTimeout(() => reject(new Error('ups')), 1000));
 const promise2 = new Promise((resolve) => setTimeout(() => resolve(2), 2000));
 const promise3 = new Promise((resolve) => setTimeout(() => resolve(3), 3000));
 
 Promise.race([promise1, promise2, promise3])
-  .then((value) => console.log(value)); // 1 setelah 1 detik
+  .then((value) => console.log(value))
+  .catch((error) => console.log(error.message)); // Ups
