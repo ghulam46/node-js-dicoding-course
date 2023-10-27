b3d9354035a9072935d67541cc575d98caa0b821
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-10-27 11:19:42 +0700

    promise race

--- chainningPromise/promiseStaticMethod.js
+++ chainningPromise/promiseStaticMethod.js
@@ -1,7 +1,14 @@
+// const promise1 = new Promise((resolve) => setTimeout(() => resolve(1), 1000));
+// const promise2 = new Promise((resolve, reject) => setTimeout(() => reject(new Error('ups')), 2000));
+// const promise3 = new Promise((resolve) => setTimeout(() => resolve(3), 3000));
+
+// Promise.all([promise1, promise2, promise3])
+//     .then((values) => console.log(values))
+//     .catch((error) => console.log(error.message));
+
 const promise1 = new Promise((resolve) => setTimeout(() => resolve(1), 1000));
-const promise2 = new Promise((resolve, reject) => setTimeout(() => reject(new Error('ups')), 2000));
+const promise2 = new Promise((resolve) => setTimeout(() => resolve(2), 2000));
 const promise3 = new Promise((resolve) => setTimeout(() => resolve(3), 3000));
 
-Promise.all([promise1, promise2, promise3])
-    .then((values) => console.log(values))
-    .catch((error) => console.log(error.message));
+Promise.race([promise1, promise2, promise3])
+  .then((value) => console.log(value)); // 1 setelah 1 detik
