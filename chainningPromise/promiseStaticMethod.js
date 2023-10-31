2bd32786741bf5eb1702c89eeea399c0bca82260
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2023-10-31 09:15:52 +0700

    promise.allSettled

--- chainningPromise/promiseStaticMethod.js
+++ chainningPromise/promiseStaticMethod.js
@@ -1,3 +1,4 @@
+// Promise All
 // const promise1 = new Promise((resolve) => setTimeout(() => resolve(1), 1000));
 // const promise2 = new Promise((resolve, reject) => setTimeout(() => reject(new Error('ups')), 2000));
 // const promise3 = new Promise((resolve) => setTimeout(() => resolve(3), 3000));
@@ -6,6 +7,8 @@
 //     .then((values) => console.log(values))
 //     .catch((error) => console.log(error.message));
 
+
+// Promise Race
 // const promise1 = new Promise((resolve) => setTimeout(() => resolve(1), 1000));
 // const promise2 = new Promise((resolve) => setTimeout(() => resolve(2), 2000));
 // const promise3 = new Promise((resolve) => setTimeout(() => resolve(3), 3000));
@@ -13,10 +16,20 @@
 // Promise.race([promise1, promise2, promise3])
 //   .then((value) => console.log(value)); // 1 setelah 1 detik
 
-const promise1 = new Promise((resolve, reject) => setTimeout(() => reject(new Error('ups')), 1000));
-const promise2 = new Promise((resolve) => setTimeout(() => resolve(2), 2000));
+// const promise1 = new Promise((resolve, reject) => setTimeout(() => reject(new Error('ups')), 1000));
+// const promise2 = new Promise((resolve) => setTimeout(() => resolve(2), 2000));
+// const promise3 = new Promise((resolve) => setTimeout(() => resolve(3), 3000));
+
+// Promise.race([promise1, promise2, promise3])
+//   .then((value) => console.log(value))
+//   .catch((error) => console.log(error.message)); // Ups
+
+
+// Promise allSettled
+const promise1 = new Promise((resolve) => setTimeout(() => resolve(1), 1000));
+const promise2 = new Promise((resolve, reject) => setTimeout(() => reject(new Error("Error")), 2000));
 const promise3 = new Promise((resolve) => setTimeout(() => resolve(3), 3000));
 
-Promise.race([promise1, promise2, promise3])
-  .then((value) => console.log(value))
-  .catch((error) => console.log(error.message)); // Ups
+Promise.allSettled([promise1, promise2, promise3])
+ .then((results) => console.log(results));
+// [{status: "fulfilled", value: 1}, {status: "rejected", reason: Error}, {status: "fulfilled", value: 3}] setelah 3 detik
